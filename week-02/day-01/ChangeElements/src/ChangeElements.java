c41bbdf413a4cab77b2f3443d45c6e40b184a1d7
lazarsoma <lazarsoma@gmail.com>
2019-04-08 15:31:38 +0200

    ChangeElements

---
+++ week-02/day-01/ChangeElements/src/ChangeElements.java
@@ -0,0 +1,14 @@
+public class ChangeElements {
+  public static void main(String[]args) {
+    int [] s = {1, 2, 3, 8, 5, 6};
+    //int count =0;
+    for (int i=0; i<s.length; i++) {
+      if (s[i] == 8) {
+        //count++;
+        s[i]=4;
+      }
+    }
+    System.out.println("The fourth element has been changed to the number "+s[3]);
+  }
+}
+
