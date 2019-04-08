b97415bb45e454c65d8e97a2612c2f7948dcb216
lazarsoma <lazarsoma@gmail.com>
2019-04-08 15:00:37 +0200

    Compare Length

---
+++ week-02/day-01/CompareLength/src/CompareLength.java
@@ -0,0 +1,10 @@
+public class CompareLength {
+  public static void main(String [] args) {
+    int [] p1 = new int [] {1, 2, 3};
+    int [] p2 = new int [] {4, 5};
+
+    if (p2.length > p1.length) {
+      System.out.println("p2 is bigger than p1");
+    }
+  }
+}
