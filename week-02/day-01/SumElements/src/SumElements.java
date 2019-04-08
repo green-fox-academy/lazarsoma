8fa23deec870ffbb6514a22123c5223e92d788b1
lazarsoma <lazarsoma@gmail.com>
2019-04-08 15:07:58 +0200

    SumElements

---
+++ week-02/day-01/SumElements/src/SumElements.java
@@ -0,0 +1,8 @@
+import static java.lang.Long.sum;
+
+public class SumElements {
+  public static void main(String [] args) {
+    int [] r = new int [] {54, 23, 66, 12};
+    System.out.println(sum( r[1], r[2]));
+  }
+}
