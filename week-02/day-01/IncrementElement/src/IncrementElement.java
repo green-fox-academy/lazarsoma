782a3ddc4a93223934983e0644185fc1a898e5be
lazarsoma <lazarsoma@gmail.com>
2019-04-08 15:52:56 +0200

    IncrementElement

---
+++ week-02/day-01/IncrementElement/src/IncrementElement.java
@@ -0,0 +1,11 @@
+public class IncrementElement {
+  public static void main(String[]args) {
+    int [] t = {1, 2, 3, 4};
+    for (int i = 0; i < t.length; i++) {
+      if (t[i] == 3) {
+       t[i] += 2;
+      }
+    }
+    System.out.println("The third element:" +t[2]);
+  }
+}
