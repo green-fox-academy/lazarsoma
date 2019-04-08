1b4ad203f3e44388139ba13550ce25cee32915f5
lazarsoma <lazarsoma@gmail.com>
2019-04-08 14:34:26 +0200

    Summing

---
+++ week-02/day-01/Summing/src/Summing.java
@@ -0,0 +1,14 @@
+public class Summing {
+  public static void main (String[] args) {
+
+    int n = 5;
+    int sum = 0;
+
+  }
+  public static int sum (int n, int sum) {
+    for (int i = 0; i <= n; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+}
