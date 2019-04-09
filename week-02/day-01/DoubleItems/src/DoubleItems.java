246fc7ebc42d47c91ce4e75441d8fa4fef5a45da
lazarsoma <lazarsoma@gmail.com>
2019-04-09 12:22:54 +0200

    SimpleReplace

---
+++ week-02/day-01/DoubleItems/src/DoubleItems.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class DoubleItems {
+  public static void main(String[] args) {
+    int[] numList = {3, 4, 5, 6, 7};
+    System.out.println(Arrays.toString(numList);
+    for (int num : numList) {
+        num = num * 2;
+      }
+    }
+}
+
+
+
