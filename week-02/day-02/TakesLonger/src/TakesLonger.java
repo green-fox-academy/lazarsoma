106c7c28c5c662b63624f9aaae538b3d69cb21a3
lazarsoma <lazarsoma@gmail.com>
2019-04-09 14:48:26 +0200

    ToDoPrint

--- week-02/day-02/TakesLonger/src/TakesLonger.java
+++ week-02/day-02/TakesLonger/src/TakesLonger.java
@@ -6,7 +6,7 @@ public class TakesLonger {
     System.out.println(sb);
   }
 }
-    
+
 
 
 
--- week-02/day-02/ToDoPrint/src/ToDoPrint.java
+++ week-02/day-02/ToDoPrint/src/ToDoPrint.java
@@ -1,18 +1,10 @@
 public class ToDoPrint {
   public static void main(String... args){
       String todoText = " - Buy milk\n";
-      // Add "My todo:" to the beginning of the todoText
-      // Add " - Download games" to the end of the todoText
-      // Add " - Diablo" to the end of the todoText but with indention
-
-      // Expected outpt:
-
-      // My todo:
-      //  - Buy milk
-      //  - Download games
-      //      - Diablo
-
-      System.out.println(todoText);
-    }
+      StringBuilder sb = new StringBuilder("- Buy milk\n");
+      sb.append("- Download games\n \t - Diablo");
+      sb.insert(0, "My todo:\n");
+      System.out.println(sb);
   }
 }
+
