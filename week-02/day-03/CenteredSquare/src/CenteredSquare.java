137d5ec2dda8fd167e70d94b7cb67cc9fd309ad4
lazarsoma <lazarsoma@gmail.com>
2019-04-10 14:39:59 +0200

    CenteredSquare

--- week-02/day-03/CenteredSquare/src/CenteredSquare.java
+++ week-02/day-03/CenteredSquare/src/CenteredSquare.java
@@ -7,15 +7,22 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class CenteredSquare {
 
   public static void mainDraw(Graphics graphics) {
+
+
     // Draw a green 10x10 square to the canvas' center.
+int x = WIDTH / 2;
+int y = HEIGHT / 2;
+square(x, y, graphics);
 
-
-
+  }
+  public static void square (int x, int y, Graphics g) {
+    g.setColor(Color.green);
+    g.drawRect(x, y, 10,10);
   }
 
   // Don't touch the code below
-  static int WIDTH = 320;
-  static int HEIGHT = 320;
+  static int WIDTH = 1920;
+  static int HEIGHT = 1080;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
