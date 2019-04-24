c73919e851ea3975f975d2c5a74d60fcf4c3c725
lazarsoma <lazarsoma@gmail.com>
2019-04-24 14:17:29 +0200

    Implement CharSequence first part

---
+++ Week-04/Day-02/Carsequence/src/Gnirts.java
@@ -0,0 +1,41 @@
+public class Gnirts implements CharSequence {
+
+  String myString;
+  CharSequence charSequence = "LazarSoma";
+
+  public Gnirts() {
+    //myString = "LazarSoma";
+
+    StringBuilder input1 = new StringBuilder();
+    input1.append("LazarSoma");
+    input1 = input1.reverse();
+    myString = input1.toString();
+
+    StringBuilder input2 = new StringBuilder();
+    input2.append("LazarSoma");
+    input2 = input2.reverse();
+    charSequence = input2.toString();
+  }
+
+  @Override
+  public int length() {
+    int result = myString.length();
+    return result;
+  }
+
+  @Override
+  public char charAt(int index) {
+    char result = myString.charAt(index);
+    return result;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+
+    CharSequence result = charSequence.subSequence(2,5);
+
+    return result;
+  }
+}
+
+
---
+++ Week-04/Day-02/Carsequence/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+
+    Gnirts g = new Gnirts();
+
+    System.out.println(g.charAt(6));
+    System.out.println(g.length());
+    System.out.println(g.subSequence(2, 5));
+
+
+
+  }
+}
