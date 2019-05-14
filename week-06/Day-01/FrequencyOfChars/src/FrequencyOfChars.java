a63d7e7117bbbed9c7848b12971dd815c011b303
lazarsoma <lazarsoma@gmail.com>
2019-05-14 17:14:54 +0200

    fixing: FrequencyOfChars

--- week-06/Day-01/FrequencyOfChars/src/FrequencyOfChars.java
+++ week-06/Day-01/FrequencyOfChars/src/FrequencyOfChars.java
@@ -1,16 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FrequencyOfChars {
-  public static void main(String[] args) {
-    String myString = "LazarSoma";
-    char[] chars = myString.toCharArray();
-    System.out.println(Arrays.asList(chars)
-            .stream()
-            .collect(Collectors.groupingBy(c -> c, Collectors.counting())));
-    //System.out.println();
+  public Map<String, Long> countChars(String input) {
+    Map<String, Long> frequentChars = Arrays.stream(input.toLowerCase().split(""))
+            .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    frequentChars.forEach((k, v) -> System.out.println(k + ":" + v));
+    return frequentChars;
   }
 }
---
+++ week-06/Day-01/FrequencyOfChars/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    FrequencyOfChars chars = new FrequencyOfChars();
+    chars.countChars("LazarSoma");
+  }
+}
