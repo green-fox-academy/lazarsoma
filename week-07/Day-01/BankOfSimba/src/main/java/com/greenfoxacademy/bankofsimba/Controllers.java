9b6a1411815f2ff620aa4ad28c1ae2620a5beb8b
lazarsoma <lazarsoma@gmail.com>
2019-05-20 14:52:11 +0200

    feature: List of Bankaccounts

--- week-07/Day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers.java
+++ week-07/Day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.bankofsimba;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Controllers {
 
+  List<BankAccount> bankAccounts = new ArrayList<>();
+
   @GetMapping("/show")
   public String account(Model model) {
-    model.addAttribute(new BankAccount("Simba",2000, "lion", "Zebra"));
+    model.addAttribute(new BankAccount("Simba", 2000, "lion", "Zebra"));
     return "index";
   }
 
@@ -19,4 +25,19 @@ public class Controllers {
     model.addAttribute("text", text);
     return "text";
   }
+
+  @ModelAttribute
+  public void setup(Model model) {
+    bankAccounts.add(new BankAccount("Timon", 1000, "meerkat", "EUR"));
+    bankAccounts.add(new BankAccount("Pumba", 1500, "warthog", "HUF"));
+    bankAccounts.add(new BankAccount("Mufasza", 3000, "lion", "EUR"));
+    bankAccounts.add(new BankAccount("Zazu", 1750, "bird", "EUR"));
+    model.addAttribute(bankAccounts);
+  }
+  @GetMapping("/list")
+  public String list (Model model) {
+    model.addAttribute(bankAccounts);
+    return "list";
+  }
 }
+
