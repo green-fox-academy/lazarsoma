51c481d58f4fed3fbbb2f7e8d7ccf804774e61ba
lazarsoma <lazarsoma@gmail.com>
2019-05-20 17:54:28 +0200

    feature: Raise the balance

--- week-07/Day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers.java
+++ week-07/Day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,13 @@ import java.util.List;
 public class Controllers {
 
   List<BankAccount> bankAccounts = new ArrayList<>();
+
+  public Controllers() {
+    bankAccounts.add(new BankAccount("Timon", 1000, "meerkat", "EUR", true, "BadGuy"));
+    bankAccounts.add(new BankAccount("Pumba", 1500, "warthog", "HUF", false, "GoodGuy"));
+    bankAccounts.add(new BankAccount("Mufasza", 3000, "lion", "EUR", true, "BadGuy"));
+    bankAccounts.add(new BankAccount("Zazu", 1750, "bird", "EUR", false, "GoodGuy"));
+  }
 
   @GetMapping("/show")
   public String account(Model model) {
@@ -28,19 +37,38 @@ public class Controllers {
 
   @ModelAttribute
   public void setup(Model model) {
-    bankAccounts.add(new BankAccount("Timon", 1000, "meerkat", "EUR", true, "BadGuy"));
-    bankAccounts.add(new BankAccount("Pumba", 1500, "warthog", "HUF", false, "GoodGuy"));
-    bankAccounts.add(new BankAccount("Mufasza", 3000, "lion", "EUR", true, "BadGuy"));
-    bankAccounts.add(new BankAccount("Zazu", 1750, "bird", "EUR", false, "GoodGuy"));
     model.addAttribute(bankAccounts);
   }
+
   @GetMapping("/list")
-  public String list (Model model) {
+  public String list(Model model) {
     model.addAttribute(bankAccounts);
     return "list";
   }
+
   @GetMapping("/othercolor")
-  public String color(){
+  public String color() {
+    return "list";
+  }
+
+  @RequestMapping(path = "/balance", method = RequestMethod.GET)
+  public String balance() {
+
+    return "list";
+  }
+
+  @RequestMapping(path = "/balance", method = RequestMethod.POST)
+  public String balance(String name) {
+    for (BankAccount b : bankAccounts) {
+      if (b.getName().equals(name)) {
+        if(b.isKing) {
+          b.balance += 100;
+        } else {
+          b.balance +=10;
+        }
+      }
+    }
+
     return "list";
   }
 }
