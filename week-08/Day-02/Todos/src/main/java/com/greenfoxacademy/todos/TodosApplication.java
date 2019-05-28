c7c6c077e93df974b4301b3f33ffa723a03e1156
lazarsoma <lazarsoma@gmail.com>
2019-05-28 14:34:02 +0200

    feature: populate database

--- week-08/Day-02/Todos/src/main/java/com/greenfoxacademy/todos/TodosApplication.java
+++ week-08/Day-02/Todos/src/main/java/com/greenfoxacademy/todos/TodosApplication.java
@@ -1,13 +1,30 @@
 package com.greenfoxacademy.todos;
 
+import com.greenfoxacademy.todos.model.Todo;
+import com.greenfoxacademy.todos.repository.ITodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosApplication {
+public class TodosApplication implements CommandLineRunner {
+
+  private ITodoRepository iTodoRepository;
+
+  public TodosApplication(ITodoRepository iTodoRepository) {
+    this.iTodoRepository = iTodoRepository;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TodosApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+    iTodoRepository.save(new Todo("Start the day"));
+    iTodoRepository.save(new Todo("Finish H2 workshop1"));
+    iTodoRepository.save(new Todo("Finish JPA workshop2"));
+    iTodoRepository.save(new Todo("Create a CRUD project"));
+  }
 }
